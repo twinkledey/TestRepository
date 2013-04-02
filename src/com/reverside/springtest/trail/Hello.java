a78d2e6433bfe244680f2db9647ed42c6326b990
twinkle <twinkle.dey@reverside.co.za>
2013-04-02 13:50:48 +0200

    Added Successfully

---
+++ src/com/reverside/springtest/trail/Hello.java
@@ -0,0 +1,14 @@
+package com.reverside.springtest.trail;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Twinkel
+ * Date: 2013/04/02
+ * Time: 1:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hello {
+    public static void main(String args[]) {
+        System.out.println("Hello");
+    }
+}
